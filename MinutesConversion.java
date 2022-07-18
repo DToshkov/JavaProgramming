77f347df8d603a65c5dbd8d4e875f47f9d5703ee
DToshkov <109114920+DToshkov@users.noreply.github.com>
2022-07-18 09:31:58 -0500

    Create MinutesConversion.java
    Prompts user for minutes, then converts the minutes into  hours and days

---
+++ MinutesConversion.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class MinutesConversion {
+    public static void main(String[] args) {
+        // Write your code here
+   Scanner input = new Scanner(System.in);
+    double days;
+    double hours;
+    int minutes;
+    double MINUTES_IN_HOUR = 60;
+    double HOURS_IN_DAYS = 24;
+
+  
+
+    System.out.println("Enter Minutes >> ");
+    minutes = input.nextInt();
+    hours = (minutes/MINUTES_IN_HOUR);
+   
+    days = ((minutes / MINUTES_IN_HOUR) / HOURS_IN_DAYS);
+
+System.out.println(minutes + " minutes is " + hours + " hours " + "or " + days +  " days." );
+    }
+}
