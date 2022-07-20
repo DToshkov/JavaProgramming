4a811555c7d09d07e84040b7b8d2dd8d95c6bbf7
DToshkov <109114920+DToshkov@users.noreply.github.com>
2022-07-19 20:10:31 -0500

    Create math.random.java
    generates three random numbers and displays them

---
+++ math.random.java
@@ -0,0 +1,11 @@
+public class math.random
+{
+  public static void main(String[] args)
+  {
+    double randomNumber;
+    randomNumber = Math.random();
+    System.out.println(randomNumber);
+    randomNumber = Math.random();
+    System.out.println(randomNumber);
+   }
+ }
