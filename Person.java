f81bb1c7bff8862fdadf3b62d0082f5c41c5b67c
DToshkov <109114920+DToshkov@users.noreply.github.com>
2022-07-28 21:37:25 -0500

    4-11 B

---
+++ Person.java
@@ -0,0 +1,23 @@
+import java.time.*;
+public class Person {
+    private String firstName;
+    private String lastName;
+    private LocalDate birthDate;
+    public Person(String firstName, String lastName, LocalDate birthDate) {
+    
+
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthDate = birthDate;
+}
+    public String getFirstName() {
+   return firstName;
+    }
+
+    public String getLastName() {
+  return lastName;
+    }
+    public LocalDate getBirthDate() {
+   return birthDate;
+    }
+}
