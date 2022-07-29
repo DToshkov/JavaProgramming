4e85e791474f721316445c61ac0d7f490a00dff7
DToshkov <109114920+DToshkov@users.noreply.github.com>
2022-07-28 21:10:44 -0500

    Create BookstoreCredit.java
    Create an application that prompts a student for a name and grade point average, and then passes the values to a method (computeDiscount) that displays a descriptive message. The message uses the student’s name, echoes the grade point average, and computes and displays the credit.

---
+++ BookstoreCredit.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class BookstoreCredit {
+    public static void main(String[] args) {
+        String name;
+        double gpa;
+        final double credits = 10;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter name: ");
+        name = sc.next();
+        System.out.println("Enter GPA: ");
+        gpa = sc.nextDouble();
+        computeDiscount(name, gpa, credits);
+    }
+
+    private static void computeDiscount(String name, double gpa, double credits) {
+
+        double total_credits = gpa * credits;
+        System.out.println(name+ ", your GPA is "+gpa+", so your credit is $"+total_credits+".");
+    }
+}
